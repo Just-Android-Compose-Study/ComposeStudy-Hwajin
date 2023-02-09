15e7b1a220da2bd624931aae5dd8dd6ea24c8466
unknown <10017019@thinkbig.co>
2023-02-09 13:28:09 +0900

    [FEAT] #1 - 환영인사 나타내기

--- Chapter01/app/src/main/java/com/example/chapter01/MainActivity.kt
+++ Chapter01/app/src/main/java/com/example/chapter01/MainActivity.kt
@@ -1,11 +1,46 @@
 package com.example.chapter01
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            Hello()
+        }
+    }
+}
+
+@Composable
+fun Welcome() {
+    Text(
+        text = stringResource(id = R.string.welcome),
+        style = MaterialTheme.typography.subtitle1
+    )
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(
+        text = stringResource(id = R.string.hello, name),
+        textAlign = TextAlign.Center,
+        style = MaterialTheme.typography.subtitle1
+    )
+}
+
+@Composable
+fun Hello() {
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Welcome()
+        Greeting(name = "hwajin")
     }
 }
