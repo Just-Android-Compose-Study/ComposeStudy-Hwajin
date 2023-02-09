cece383f81333f1b86868c217d894600a4be5478
hjh1161514 <hjh1161514@naver.com>
2023-02-09 14:46:37 +0900

    [FEAT] #3 - 클릭 이벤트

--- Chapter02/app/src/main/java/com/example/chapter02/MainActivity.kt
+++ Chapter02/app/src/main/java/com/example/chapter02/MainActivity.kt
@@ -5,14 +5,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.DropdownMenu
-import androidx.compose.material.DropdownMenuItem
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,27 +30,30 @@ fun Factorial() {
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
-        Text(
-            modifier = Modifier.clickable {
-                expanded = true // 드롭다운 메뉴가 화면에 즉시 나타남. false하면 클릭해도 아무런 일이 없음
-            },
-            text = text,
-            style = MaterialTheme.typography.h2
-        )
-        DropdownMenu(
-            expanded = expanded,
-            onDismissRequest = { // 아무것도 선택하지 않고 닫으면
-                expanded = false // true면 드롭박스 이외를 클릭해도 화면이 계속 나타남
-            }
-        ) {
-            for (n in 0 until 10) {
-                DropdownMenuItem(onClick = {
-                    expanded = false
-                    text = factorialAsString(n)
-                }) {
-                    Text("$n!")
+        Column() {
+            Text(
+                modifier = Modifier.clickable {
+                    expanded = true // 드롭다운 메뉴가 화면에 즉시 나타남. false하면 클릭해도 아무런 일이 없음
+                },
+                text = text,
+                style = MaterialTheme.typography.h2
+            )
+            DropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { // 아무것도 선택하지 않고 닫으면
+                    expanded = false // true면 드롭박스 이외를 클릭해도 화면이 계속 나타남
+                }
+            ) {
+                for (n in 0 until 10) {
+                    DropdownMenuItem(onClick = {
+                        expanded = false
+                        text = factorialAsString(n)
+                    }) {
+                        Text("$n!")
+                    }
                 }
             }
+            ButtonDemo()
         }
     }
 }
@@ -62,4 +64,19 @@ fun factorialAsString(n: Int): String {
         result *= i
     }
     return "$n! = $result"
+}
+
+@Composable
+@Preview
+fun ButtonDemo() {
+    Box {
+        Button(
+            onClick = {
+                println("눌림")
+            },
+            // enabled = false
+        ) {
+            Text("여기를 누르세요")
+        }
+    }
 }
